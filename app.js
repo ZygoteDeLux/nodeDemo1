f1c0ea1a8a8035770f81f12f607022520b3f48d1
oscarstorm <oscar.emil.storm@gmail.com>
2023-09-09 23:15:21 +0200

    Solved full crud for mountains

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mountains = [
 
 app.get("/mountains", (req, res) => {
 
-    res.send(mountains);
+    res.send({data: mountains});
 
 });
 
@@ -20,13 +20,120 @@ app.get("/mountains/:id", (req, res) => {
     const mountain = mountains.find(m => m.id === mountainId)
 
     if (mountain) {
-        res.send(mountain);
+        res.send({data: mountain});
       } else {
         res.status(404).send('Mountain not found');
       }
-      
+    
 
-})
+});
+
+//?id=?&name=Himmelbjerget&height=147
 
 
-app.listen(8080);
+app.post("/mountains", (req, res) => {
+
+  //deconstruct query
+  const { id, name, height } = req.query; 
+
+    if (id && name && height) {
+
+      //convert id and height to numbers
+      const mountainId = parseInt(id, 10);
+      const mountainHeight = parseInt(height, 10);
+
+      //check if a mountain with the same id already exists
+      const existingMountain = mountains.find(m => m.id === mountainId);
+
+      if (existingMountain) {
+        res.status(400).send('Mountain with the same ID already exists');
+      } 
+      else {
+        const newMountain = { id: mountainId, name, height: mountainHeight };
+        mountains.push(newMountain);
+        res.status(201).send({ data: newMountain });
+    }
+  } 
+    else {
+    res.status(400).send('Invalid request parameters');
+  }
+
+});
+
+
+app.patch("/mountains", (req, res) => {
+
+  //deconstruct query
+  const {id, name, height} = req.query;
+
+  //if id, name or height 
+  if(id && name || height){
+
+    //convert id and height to numbers
+    const mountainId = parseInt(id, 10);
+    const mountainHeight = parseInt(height, 10);
+
+    //find the index of the object in the mountains array with the corosponding id.
+    const index = mountains.findIndex(m => m.id === mountainId);
+
+    //if the index exists
+    if(index !== -1){
+      //checks if the name parameter of the query exists, 
+      //it then sets the object of the given index name to the query parameter
+      if(name) mountains[index].name = name;
+      //checks if the height parameter of the query exists,
+      //it then sets the object of the given index height to the query parameter
+      if(height) mountains[index].height = mountainHeight;
+      //sends the updated object
+      res.send({data : mountains[index]});
+    } 
+    else {
+      res.status(404).send('Mountain not found');
+    }
+  }
+  else {
+    res.status(400).send('Invalid request parameters');
+  }
+  
+});
+
+app.delete("/mountains", (req, res) => {
+
+  //deconstruct query
+  const { id } = req.query; 
+
+  //if id exists
+  if (id) {
+
+    const mountainId = parseInt(id, 10);
+
+    //index of the given mountain found by id.
+    const index = mountains.findIndex(m => m.id === mountainId);
+
+    //if index of the object exists
+    if (index !== -1) {
+      //delete the object and assigns it to deletedMountains
+      const deletedMountains = mountains.splice(index, 1)[0];
+      //sends the deletedMountains objects to client
+      res.send({ data: deletedMountains });
+    } else {
+      res.status(404).send('Mountain not found');
+    }
+  } else {
+    res.status(400).send('Invalid request parameters');
+  }
+});
+
+
+
+const PORT = 8080;
+
+app.listen(PORT, (error) =>{
+
+  if(error){
+    console.log("Error starting the server", error);
+    return;
+  }
+
+  console.log("Server is running on port", PORT)
+});
